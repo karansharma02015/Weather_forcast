6b3e06e21278c949456a661743ec483a97ad8655
Karan <karansharma02015@gmail.com>
2023-08-28 15:36:32 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Weather from './Component/Weather';
+import Weather from './Project/Weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
